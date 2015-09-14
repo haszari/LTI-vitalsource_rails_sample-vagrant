56678b81938bea6d72b1a39889015750e31a1007
haszari <haszari@cartoonbeats.com>
2015-09-15 10:21:05 +1200

    vagrant setup for rails LTI sample code

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y rails
+apt-get install -y git
+apt-get install -y libmysqlclient-dev
+apt-get install -y sqlite3
+apt-get install -y libsqlite3-dev
